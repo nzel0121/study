6d2a997693979ac2230839d37257d6d34a0219a7
Terry Lee <2tk.java@gmail.com>
2019-03-07 15:49:34 +0900

    Hello World test code

--- java/microserviceSecondEdition/src/test/java/tklee/study/microservice2e/Microservice2eApplicationTests.java
+++ java/microserviceSecondEdition/src/test/java/tklee/study/microservice2e/Microservice2eApplicationTests.java
@@ -1,16 +1,33 @@
 package tklee.study.microservice2e;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Microservice2eApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void contextLoads() {
+    public void testSpringBootApp() throws IOException {
+        String body = restTemplate.getForObject("/", String.class);
+        assertThat(
+                new ObjectMapper().readTree(body)
+                        .get("message")
+                        .textValue()
+                )
+                .isEqualTo("Hello World");
     }
 
 }
