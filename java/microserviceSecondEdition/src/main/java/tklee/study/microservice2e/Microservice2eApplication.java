ff85716f79a8b7f9146c4563536a7be1b7b7ee46
Terry Lee <2tk.java@gmail.com>
2019-03-07 15:43:54 +0900

    @RestController

--- java/microserviceSecondEdition/src/main/java/tklee/study/microservice2e/Microservice2eApplication.java
+++ java/microserviceSecondEdition/src/main/java/tklee/study/microservice2e/Microservice2eApplication.java
@@ -2,6 +2,8 @@ package tklee.study.microservice2e;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Microservice2eApplication {
@@ -10,4 +12,30 @@ public class Microservice2eApplication {
         SpringApplication.run(Microservice2eApplication.class, args);
     }
 
+}
+
+@RestController
+class GreetingController {
+    @GetMapping("/")
+    Greet greet(){
+        return new Greet("Hello World");
+    }
+}
+
+class Greet{
+    private String message;
+
+    public Greet(){}
+
+    public Greet(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
