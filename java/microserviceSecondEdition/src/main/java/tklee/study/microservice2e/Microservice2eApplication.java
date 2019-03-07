62644e34c14598ab5a2ef89fb09ce4e624ccb23b
Terry Lee <2tk.java@gmail.com>
2019-03-07 15:28:53 +0900

    initial project for study with book that microservice 2nd edition

---
+++ java/microserviceSecondEdition/src/main/java/tklee/study/microservice2e/Microservice2eApplication.java
@@ -0,0 +1,13 @@
+package tklee.study.microservice2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice2eApplication.class, args);
+    }
+
+}
